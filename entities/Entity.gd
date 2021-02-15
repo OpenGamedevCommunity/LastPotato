b20fa8e42fc85706b2a4fbe7428a45a646036c22
nezvers <agnis16@inbox.lv>
2021-02-15 23:42:58 +0200

    Multi-character animations and movement
    Thanks to lucky asset choice we have multiple player characters that can be chosen.
    Entity inheritance for the movement

--- entities/Entity.gd
+++ entities/Entity.gd
@@ -1,2 +1,31 @@
 extends KinematicBody2D
 class_name Entity
+
+
+
+export(float) var speed: = 1.0 * 60
+var dir: = Vector2.ZERO
+var velocity: = Vector2.ZERO
+
+
+onready var body:Node2D = $Body
+onready var sprite:Sprite = $Body/Sprite
+onready var spritePlayer:AnimationPlayer = $Body/SpritePlayer
+
+#inherited entities decide the implementation
+func set_dir()->void:
+	pass
+
+func _physics_process(_delta:float)->void:
+	set_dir()
+	velocity = dir * speed
+	velocity = move_and_slide(velocity)
+
+func _process(_delta:float)->void:
+	if abs(dir.x) > 0.01:
+		sprite.flip_h = dir.x < 0.0
+	set_animation()
+
+#inherited entities decide the implementation
+func set_animation()->void:
+	pass
--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -1,2 +1,35 @@
 extends Entity
 
+export(String) var ctrl = '1'
+
+enum CharList{
+	ROGUE_FEMALE,
+	ROGUE_MALE,
+	KNIGHTT_FEMALE,
+	KNIGHT_MALE,
+	MAGE_FEMALE,
+	MAGE_MALE
+}
+export(CharList) var character: = 1
+
+enum {IDLE,RUN}
+var animList: = [
+	["rogue_f_idle",	"rogue_f_run"],
+	["rogue_m_idle",	"rogue_m_run"],
+	["knight_f_idle",	"knight_f_run"],
+	["knight_m_idle",	"knight_m_run"],
+	["mage_f_idle",		"mage_f_run"],
+	["mage_m_idle",		"mage_m_run"],
+]
+
+
+func set_dir()->void:
+	dir.x = Input.get_action_strength("right_p"+ctrl) - Input.get_action_strength("left_p"+ctrl)
+	dir.y = Input.get_action_strength("down_p"+ctrl) - Input.get_action_strength("up_p"+ctrl)
+	dir = dir.normalized()
+
+func set_animation()->void:
+	if dir.length() > 0.1:
+		spritePlayer.play(animList[character][RUN])
+	else:
+		spritePlayer.play(animList[character][IDLE])
