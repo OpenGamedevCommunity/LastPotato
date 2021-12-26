1ef8d4ba1fe45ff653e9e2bec8b2674177288cd4
nezvers <agnis16@gmail.com>
2021-12-26 20:04:01 +0200

    draw camera points

--- objects/Camera.gd
+++ objects/Camera.gd
@@ -13,3 +13,28 @@ func _physics_process(delta:float)->void:
 	if owner.playerList.size() > 0:
 		pos = pos.linear_interpolate(middle, 8.0*delta)
 		global_position = pos.round()
+
+
+
+
+
+
+
+
+# VISUALIZATION
+
+var draw: = false
+func _process(_delta:float)->void:
+	update()
+
+func _draw()->void:
+	if !draw:
+		return
+	draw_circle(Vector2.ZERO, 2, Color.white)
+	for t in owner.playerList:
+		draw_line(t.global_position - global_position, Vector2.ZERO, Color.white)
+
+func _input(event:InputEvent)->void:
+	if event.is_action_pressed("ui_accept"):
+		draw = !draw
+		update()
