9d29a4c9cc44c86822ab0f02098ddcd56c5b3df4
Agnis <agnis16@gmail.com>
2022-01-02 00:13:58 +0200

    post tutorial

--- objects/Camera.gd
+++ objects/Camera.gd
@@ -6,6 +6,21 @@ export (float) var lerpSpeed: = 8.0
 onready var pos: = global_position
 #Camera needs to be calculated on the same thread as the player movement
 #otherwise player sprite is unstable
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 func _process(delta:float)->void:
 	if followList.list.empty():
 		return
@@ -17,9 +32,6 @@ func _process(delta:float)->void:
 	
 	pos = pos.linear_interpolate(middle, lerpSpeed*delta)
 	global_position = pos.round()
-	
-	if !draw:
-		update()
 
 
 
@@ -27,17 +39,11 @@ func _process(delta:float)->void:
 
 
 
-# VISUALIZATION
 
-var draw: = false
 
-func _draw()->void:
-	if !draw:
-		return
-	draw_circle(Vector2.ZERO, 2, Color.white)
-	for t in followList.list:
-		draw_line(t.global_position - global_position, Vector2.ZERO, Color.white)
 
-func _input(event:InputEvent)->void:
-	if event.is_action_pressed("ui_accept"):
-		draw = !draw
+
+
+
+
+
--- resources/Subscription.gd
+++ resources/Subscription.gd
@@ -2,3 +2,4 @@ extends Resource
 class_name Subscription
 
 export (Array) var list
+
